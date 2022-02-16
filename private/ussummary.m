f323b652b66a43091de755dc84cac805311931c2
Stephen Obrochta <stevicuss@gmail.com>
2022-02-16 12:06:40 +0900

    percent and grain data low uncertainty >0

--- private/ussummary.m
+++ private/ussummary.m
@@ -1,6 +1,6 @@
-function [summarymat, shadingmat, depthrange] = ussummary(depthstart, depthend, nsim, agedepmat, interpinterval, inputfile, writedir, bootpc, xfactor, depthcombine, savebigmat, proxy, SaveName)
-%--- Summarise the data agedepmat data to discrete depth probabilities
+function [summarymat, shadingmat, depthrange] = ussummary(depthstart, depthend, nsim, agedepmat, interpinterval, writedir, bootpc, xfactor, depthcombine, savebigmat, proxy, SaveName)
 
+%--- Summarise the data agedepmat data to discrete depth probabilities
 depthrange = [depthstart ceil(depthstart):interpinterval:floor(depthend) depthend]';
 depthrange = unique(depthrange);
 if max(depthrange) < depthend
@@ -18,7 +18,7 @@ tempage = NaN(length(depthrange),nsim);
 
 % attempt to use faster precompiled binary
 try
-	checkmex = sum(nakeinterp1([1; 10],[1; 100],[2:2:9]')) == 180;
+	checkmex = sum(nakeinterp1([1; 10],[1; 100],(2:2:9)')) == 180;
 catch err
 end
 
@@ -48,33 +48,33 @@ if exist('checkmex','var') == 1
 	end
 end
 
-% percent and per gram can't be <0
-% if contains(proxy,'PER') || contains(proxy,'PGR')
-% 	index = tempage < 0;
-% 	tempage(index) = 0;
-% end
-
 % create probability density cloud for ages
 allprctiles = prctile(tempage,[1:99, 100*(1-erf(2/sqrt(2)))/2, 100*(1-erf(1/sqrt(2)))/2, 100-100*(1-erf(1/sqrt(2)))/2, 100-100*(1-erf(2/sqrt(2)))/2] , 2);
+
+% percent and per gram can't be <0
+if contains(proxy,'PER') || contains(proxy,'PGR')
+	index = allprctiles < 0;
+	allprctiles(index) = 0;
+end
+
+% summarize and create matrix from probability density
 shadingmat = allprctiles(:,1:99);
 summarymat = [allprctiles(:,[50, 100:end]), nanmean(tempage,2)]; % summarymat is: median, 2siglo, 1siglo, 1sighi, 2sighi, mean
 
 % save to disk
-
-
 if depthcombine == 1
 	comtag = 'Yes';
 elseif depthcombine == 0
 	comtag = 'No'; 
 end
 
-% fid_output = fopen(SaveName,'w');
-% fprintf(fid_output,'%s',['Undatable run on ',datestr(now,31),'. nsim=',num2str(nsim),' bootpc=',num2str(bootpc,'%.2g'),' xfactor=',num2str(xfactor,'%.2g'),' combine=',comtag]);
-% fprintf(fid_output,'\r\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s','Depth','Median age','Mean age','95.4% age','68.2% age','68.2% age','95.4% age');
-% for i = 1:size(depthrange,1)
-% 	fprintf(fid_output,'\r\n%f\t%.0f\t%.0f\t%.0f\t%.0f\t%.0f\t%.0f',depthrange(i),summarymat(i,1),summarymat(i,6),summarymat(i,2),summarymat(i,3),summarymat(i,4),summarymat(i,5));
-% end
-% fclose(fid_output);
+fid_output = fopen(SaveName,'w');
+fprintf(fid_output,'%s',['Undatable run on ',datestr(now,31),'. nsim=',num2str(nsim),' bootpc=',num2str(bootpc,'%.2g'),' xfactor=',num2str(xfactor,'%.2g'),' combine=',comtag]);
+fprintf(fid_output,'\r\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s','Depth','Median age','Mean age','95.4% age','68.2% age','68.2% age','95.4% age');
+for i = 1:size(depthrange,1)
+	fprintf(fid_output,'\r\n%f\t%.0f\t%.0f\t%.0f\t%.0f\t%.0f\t%.0f',depthrange(i),summarymat(i,1),summarymat(i,6),summarymat(i,2),summarymat(i,3),summarymat(i,4),summarymat(i,5));
+end
+fclose(fid_output);
 
 
 % Save output (if savemat selected)
