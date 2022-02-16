abd7ebc1c0e9c23ceec51cda38352f7dd6081402
Stephen Obrochta <stevicuss@gmail.com>
2022-02-16 14:16:46 +0900

    adjusted size of printed PDF

--- usplotoptions.m
+++ usplotoptions.m
@@ -1,17 +1,33 @@
-%% settings for the age-depth plot
-% set plot size here
-plotwidth  = 18    ; % centimetres
-plotheight = 18    ; % centimetres
-% set font size here
-textsize   = 8     ; % font size
-% set axis labels here (e.g. change to metres or Ma if you wish)
-depthlabel = 'Depth (cm)';
+% plot dimensions in pixels
+figw = 600;
+axesl = 55;
+axesw = 525;
+figh = 400;
+axesb = 50;
+axesh = 310;
+
+% set paper size
+plotwidth = 18; % centimetres
+plotheight = plotwidth * (figh / figw);
+
+% set font size
+textsize = 12;
+
+% set axis labels (e.g. change to metres or Ma if you wish)
 agelabel =  'Age (ka)';
 
-%% settings for the SAR plot
-if exist('plotsar','var') == 1
-	if plotsar == 1
-		plotheight = plotheight * 1.5;	% The total height of both plots relative to plotheight to also accomodate SAR plot
-	end
+% convert pixel dimensions to relative units so plot can be resized
+axesl = axesl / figw;
+axesb = axesb / figh;
+axesw = axesw / figw;
+axesh = axesh / figh;
+
+% center figure on primary monitor
+scrnsze = get(0,'monitorPositions');
+if size(scrnsze,1) > 1
+	i = find(scrnsze(:,1) == 1);
+else
+	i = 1;
 end
-sarlabel = 'SAR (cm kyr^{-1})';
+figl = (scrnsze(i,3)/2) - figw / 2;
+figb = (scrnsze(i,4)/2) - figh / 2;
